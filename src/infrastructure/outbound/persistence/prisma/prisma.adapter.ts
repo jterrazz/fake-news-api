0bd1a4a758210214dcb0ffd35d047b126c06f1bd
Jean-Baptiste Terrazzoni <contact@jterrazz.com>
2025-04-26 19:51:08 +0200

    refactor: enhance PrismaAdapter with logging capabilities
    - Introduce LoggerPort dependency to PrismaAdapter for improved error, warning, info, and query logging.
    - Implement event listeners for Prisma log events to capture and log relevant messages, enhancing observability and debugging capabilities.

--- src/infrastructure/outbound/persistence/prisma/prisma.adapter.ts
+++ src/infrastructure/outbound/persistence/prisma/prisma.adapter.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { type LoggerPort } from '@jterrazz/logger';
+import { type Prisma, PrismaClient } from '@prisma/client';
 
 import { type DatabasePort } from '../../../../application/ports/outbound/persistence/database.port.js';
 
@@ -6,10 +7,51 @@ export class PrismaAdapter implements DatabasePort {
     private static instance: null | PrismaClient = null;
     private client: PrismaClient;
 
-    constructor() {
+    constructor(private readonly logger: LoggerPort) {
         if (!PrismaAdapter.instance) {
             PrismaAdapter.instance = new PrismaClient({
-                log: ['error', 'warn'],
+                log: [
+                    {
+                        emit: 'event',
+                        level: 'query',
+                    },
+                    {
+                        emit: 'event',
+                        level: 'error',
+                    },
+                    {
+                        emit: 'event',
+                        level: 'info',
+                    },
+                    {
+                        emit: 'event',
+                        level: 'warn',
+                    },
+                ],
+            });
+
+            PrismaAdapter.instance.$on('error' as never, (event: Prisma.LogEvent) => {
+                this.logger.error(`Prisma error: ${event.message}`, {
+                    ...event,
+                });
+            });
+
+            PrismaAdapter.instance.$on('warn' as never, (event: Prisma.LogEvent) => {
+                this.logger.warn(`Prisma warning: ${event.message}`, {
+                    ...event,
+                });
+            });
+
+            PrismaAdapter.instance.$on('info' as never, (event: Prisma.LogEvent) => {
+                this.logger.info(`Prisma info: ${event.message}`, {
+                    ...event,
+                });
+            });
+
+            PrismaAdapter.instance.$on('query' as never, (event: Prisma.LogEvent) => {
+                this.logger.info(`Prisma query: ${event.message}`, {
+                    ...event,
+                });
             });
         }
         this.client = PrismaAdapter.instance;
